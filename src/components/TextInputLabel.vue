45eb485ba277a4fcf646316974fc68964e724d7b
Rob Donnelly <rfdonnelly@gmail.com>
2023-05-14 20:40:07 -0700

    poc

---
+++ src/components/TextInputLabel.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    label: string,
+    modelValue: string,
+  }>();
+  defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <div class="relative">
+    <input
+      id="input"
+      type="text"
+      :value="props.modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="
+        peer
+        font-mono
+        h-10
+        w-full
+        border-0
+        outline-none
+        px-2
+      "
+      />
+    <label for="input"
+      class="
+        absolute
+        left-0
+        -top-3.5
+        text-sm
+      "
+      >{{props.label}}
+    </label>
+  </div>
+</template>
--- src/components/WebSerialUart.vue
+++ src/components/WebSerialUart.vue
@@ -1,85 +1,122 @@
 <script setup lang="ts">
+import TextInputLabel from './TextInputLabel.vue';
 import { Request } from '../request.ts';
 import { RequestEncoder } from '../request_encoder.ts';
+import { ResponseDecoder } from '../response_decoder.ts';
 import { ref } from 'vue';
 
-const tx_log = ref("");
-const addr = ref(0);
-const write_data = ref(0);
+const log = ref("");
+const addr = ref("0x00000000");
+const data = ref("0x55555555");
+const port = ref(null);
 
-function send_write() {
-  send_request({
+async function connect() {
+  port.value = await navigator.serial.requestPort();
+  await port.value.open({
+    baudRate: 115200,
+  });
+}
+
+async function send_write() {
+  await send_request({
     command: "Write",
-    addr: addr.value,
-    data: write_data.value,
+    addr: parseInt(addr.value),
+    data: parseInt(data.value),
     crc: 0,
   });
 }
 
-function send_read() {
-  send_request({
+async function send_read() {
+  await send_request({
     command: "Read",
-    addr: addr.value,
+    addr: parseInt(addr.value),
     crc: 0,
   });
 }
 
-function send_request(request: Request) {
+async function send_request(request: Request) {
     const encoder = new RequestEncoder();
     const bytes = encoder.encode(request);
-    tx_log.value += JSON.stringify(request);
-    tx_log.value += "\n";
-    tx_log.value += bytes;
-    tx_log.value += "\n";
+    log.value += new Date(Date.now()).toISOString() + "\n";
+    log.value += JSON.stringify(request) + "\n";
+    log.value += bytes + "\n";
+
+    if (port.value && port.value.writeable) {
+      const writer = port.value.writeable.getWriter();
+      await writer.write(bytes);
+      writer.releaseLock();
+    }
+
+    if (port.value && port.value.readable) {
+      const reader = port.value.readable
+        .pipeThrough(new TransformStream(new ResponseDecoder()))
+        .getReader();
+
+      const {value, done} = await reader.read();
+      const response = value;
+      if (response.command == "Read") {
+        data.value = response.data;
+      }
+      log.value += new Date(Date.now()).toISOString() + "\n";
+      log.value += JSON.stringify(response) + "\n";
+      reader.releaseLock();
+    }
 }
 
 </script>
 
 <template>
-  <div class="grid gap-4 grid-cols-2">
-    <div id="tx grid grid-rows-2">
-      <div>
-        <div class="relative">
-          <input id="addr" type="text" :value="addr"
-            class="peer block min-h-[auto] w-full rounded border-0 bg-neutral-100 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:bg-neutral-700 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-          >
-          <label for="addr"
-            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out -translate-y-[0.9rem] scale-[0.8] text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-          >
-            Address
-          </label>
-        </div>
-        <div class="relative">
-          <input id="write_data" type="text" :value="write_data"
-            class="peer block min-h-[auto] w-full rounded border-0 bg-neutral-100 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:bg-neutral-700 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-          >
-          <label for="write_data"
-            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out -translate-y-[0.9rem] scale-[0.8] text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-          >
-            Data
-          </label>
-        </div>
-        <button type="button" class="bg-sky-500 rounded-l-lg rounded-r-none" @click="send_write">W</button>
-        <button type="button" class="bg-sky-500 rounded-l-none rounded-r-lg" @click="send_read">R</button>
+  <div class="flex flex-row gap-4 py-10">
+    <button type="button"
+      class="
+        bg-sky-500 rounded-lg p-2
+      "
+      @click="connect"
+    >Connect</button>
+  </div>
+    <div id="tx">
+      <div class="flex flex-row py-2 gap-4">
+        <TextInputLabel class="w-1/2" label="Address" v-model="addr"/>
+        <TextInputLabel class="w-1/2" label="Data" v-model="data"/>
       </div>
-      <div>
-        <label for="tx_log">Log</label>
-        <textarea id="tx_log" rows="4" class="block font-mono w-full">{{ tx_log }}</textarea>
+      <div class="flex flex-row">
+        <button type="button"
+          class="
+            p-2 bg-sky-500 rounded-l-lg rounded-r-none
+          "
+          @click="send_write"
+        >W</button>
+        <button type="button"
+          class="
+            p-2 bg-sky-500 rounded-l-none rounded-r-lg
+          "
+          @click="send_read"
+        >R</button>
       </div>
-    </div>
-    <div id="rx">
-      <div class="relative">
-        <input id="read_data" type="text" :value="read_data"
-          class="peer block min-h-[auto] w-full rounded border-0 bg-neutral-100 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:bg-neutral-700 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+      <div class="relative my-10">
+        <textarea
+          id="log"
+          rows="12"
+          class="
+            block
+            font-mono
+            w-full
+            border-0
+            outline-none
+            p-2
+          "
+        >{{ log }}</textarea>
+        <label
+          for="log"
+          class="
+            absolute
+            left-0
+            -top-3.5
+            text-sm
+          "
         >
-        <label for="read_data"
-          class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out -translate-y-[0.9rem] scale-[0.8] text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-        >
-          Data
+          Log
         </label>
       </div>
-      <label for="rx_log">Log</label>
-      <textarea id="rx_log" rows="4" class="block font-mono w-full">{{ rx_log }}</textarea>
     </div>
-  </div>
 </template>
